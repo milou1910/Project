e9a73b8e5f09586a9d1cc11f7f273a835c81716d
milou1910 <94874504+milou190@users.noreply.github.com>
2022-06-17 15:26:27 +0200

    modif

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
     
-    /**
-     * @Route("/hello", name="hello_world")
-     */
-    public function hello(Request $request)
+    #[Route(path:'/hello', name: 'blog')]
+
+    public function hello()
     {
 
         $response = new Response ("hello helloooo");
